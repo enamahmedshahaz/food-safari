738c1c45b16c84deea97ffedc2c499017d7774d7
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-09-16 15:26:31 +0600

    Show selected menu with red border

--- js/food.js
+++ js/food.js
@@ -1,11 +1,29 @@
 let defaultCategory = 'Beef';
+let previousClickedAnchor = null;
+
+window.onload = function () {
+    // Select the first <a> element and set its color to red by default
+    const firstAnchor = document.querySelector('ul.menu-horizontal a');
+
+    firstAnchor.classList = 'border-2 border-red-500';
+    previousClickedAnchor = firstAnchor;  // Set the first anchor as the previously clicked one
+}
+
+
+const showLoadingAnimation = (show) => {
+    let loading = document.getElementById("loading");
+    if (show) {
+        loading.innerHTML = `<span class="loading loading-spinner loading-lg"></span>`;
+    } else {
+        loading.innerHTML = '';
+    }
+}
 
 const loadFood = (category) => {
 
-    let loading = document.getElementById("loading");
-    loading.innerHTML = `<span class="loading loading-spinner loading-lg"></span>`;
+    showLoadingAnimation(true);
 
-    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${category}`
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${category}`;
     fetch(url)
         .then(res => res.json())
         .then(data => showMeals(data.meals))
@@ -36,15 +54,22 @@ const showMeals = (meals) => {
         container.appendChild(mealCard);
     });
 
-    let loading = document.getElementById("loading");
-
-    loading.innerHTML = '';
-
+    showLoadingAnimation(false);
 }
 
 
+
 const categoryListClickHandler = (event) => {
-    let categoryName = event.target.innerText;
-    // console.log('Clicked: ', categoryName);
+
+    const clickedAnchor = event.target;
+    const categoryName = event.target.innerText;
+
+    if (previousClickedAnchor) {
+        previousClickedAnchor.classList = '';
+    }
+    clickedAnchor.classList = 'border-2 border-red-500';
+
+    previousClickedAnchor = clickedAnchor;
+
     loadFood(categoryName);
 }
